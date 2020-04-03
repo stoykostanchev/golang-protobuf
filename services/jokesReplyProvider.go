0ab788bc10434750ce051ff6dfb670677ff12d92
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-03 16:42:29 +0100

    Add the actual API

--- services/jokesReplyProvider.go
+++ services/jokesReplyProvider.go
@@ -4,15 +4,80 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"sync"
 	"time"
 
 	pb "github.com/backend/generated/proto"
+	jsonpb "github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 )
 
 const cachePath = "images.cache"
 const capacity = 1000
+
+/*JokesAPI is a type that talks to the remote jokes API*/
+type JokesAPI struct {
+	baseURL string
+}
+
+/*GetJokesAPI returns an intance of the jokesAPI, which should be used to talk to the remote server */
+func GetJokesAPI() *JokesAPI {
+	return &JokesAPI{baseURL: "https://official-joke-api.appspot.com/jokes"}
+}
+
+/*GetJoke returns a random joke*/
+func (api *JokesAPI) GetJoke() (*pb.Joke, error) {
+	resp, err := http.Get(api.baseURL + "/programming/random")
+	joke := &pb.Joke{}
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	stringifiedBody := string(body)
+	// the response is an invalid json - obj wrapped in an arr [{...}]
+	r := stringifiedBody[1:(len(stringifiedBody) - 1)]
+
+	err2 := jsonpb.UnmarshalString(r, joke)
+
+	if err2 != nil {
+		fmt.Println(err2)
+		return nil, err
+	}
+	return joke, nil
+}
+
+/*GetRandomJokes fetches a list of random jokes*/
+func (api *JokesAPI) GetRandomJokes() *pb.JokesReply {
+	resp, err := http.Get(api.baseURL + "/programming/ten")
+	reply := &pb.JokesReply{Jokes: []*pb.Joke{}}
+
+	if err != nil {
+		fmt.Println(err)
+		return reply
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return reply
+	}
+	// Response body is an invalid json - an array of jokes
+	stringifiedBody := "{ \"jokes\": " + string(body) + "}"
+
+	err2 := jsonpb.UnmarshalString(stringifiedBody, reply)
+
+	if err2 != nil {
+		fmt.Println(err2)
+		return reply
+	}
+	return reply
+}
 
 func loadPersistantReply(message proto.Message, cachePath string) error {
 	data, err := ioutil.ReadFile(cachePath)
@@ -29,10 +94,11 @@ func loadPersistantReply(message proto.Message, cachePath string) error {
 
 /*JokesReplyProvider stores jokes in memory*/
 type JokesReplyProvider struct {
-	reply            pb.JokesReply
+	reply            *pb.JokesReply
 	newJokesCh       chan *pb.Joke
 	cachePath        string
 	dummyJokeCounter int32
+	jokesAPI         *JokesAPI
 }
 
 var singleton *JokesReplyProvider
@@ -41,16 +107,18 @@ var once sync.Once
 /*GetJokesReplyProvider returns a singleton that caches jokes in-memory and can fetch new jokes from the web*/
 func GetJokesReplyProvider() *JokesReplyProvider {
 	once.Do(func() {
-		reply := pb.JokesReply{Jokes: []*pb.Joke{}}
-		e := loadPersistantReply(&reply, cachePath)
+		api := GetJokesAPI()
+		reply := &pb.JokesReply{Jokes: []*pb.Joke{}}
+		e := loadPersistantReply(reply, cachePath)
 		if e != nil {
-			fmt.Println("Err loading")
+			reply = api.GetRandomJokes()
 		}
 		singleton = &JokesReplyProvider{
 			reply:            reply,
 			newJokesCh:       make(chan *pb.Joke, capacity),
 			cachePath:        cachePath,
-			dummyJokeCounter: 0}
+			dummyJokeCounter: 0,
+			jokesAPI:         api}
 	})
 	return singleton
 }
@@ -67,7 +135,7 @@ forLoop:
 			break forLoop
 		}
 	}
-	return &rp.reply
+	return rp.reply
 }
 
 /*StartRegularJokeUpdates spawns a new thread and stars re-fetching every SECS seconds*/
@@ -75,27 +143,23 @@ func (rp *JokesReplyProvider) StartRegularJokeUpdates(secs time.Duration) {
 	fmt.Println("Timer starting")
 	go func(c chan *pb.Joke) {
 		for range time.Tick(time.Second * secs) {
-			joke := rp.getJoke()
-			fmt.Println("Tick. Got a new joke.", joke.GetId())
+			joke, err := rp.jokesAPI.GetJoke()
+			if err != nil {
+				fmt.Println("Non nil error for getting a joke", err)
+				return
+			}
+			fmt.Println("Got a new joke.", joke)
 			jr := pb.JokesReply{}
 			e := loadPersistantReply(&jr, rp.cachePath)
 
 			if e != nil || !rp.isJokePersistant(joke, &jr) {
 				rp.persistJoke(joke, &jr)
-				fmt.Println("Persisted")
 			} else {
 				fmt.Println("Failed to persist or joke did not need persisting")
 			}
 			c <- joke
 		}
 	}(rp.newJokesCh)
-}
-
-func (rp *JokesReplyProvider) getJoke() *pb.Joke {
-	rp.dummyJokeCounter++
-	joke := pb.Joke{Id: rp.dummyJokeCounter, Type: "programming", Setup: "Knock Knock?", Punchline: "Yo mamma"}
-
-	return &joke
 }
 
 func (rp *JokesReplyProvider) isJokePersistant(joke *pb.Joke, reply *pb.JokesReply) bool {
